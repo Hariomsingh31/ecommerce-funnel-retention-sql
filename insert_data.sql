3fbdfcbd38823227ceb35f2cc3df1638bad4a2e8
Hariom singh <127221639+Hariomsingh31@users.noreply.github.com>
2026-02-20 13:39:05 +0530

    Add initial data to insert_data.sql
    Insert initial data for customers, website events, and orders.

---
+++ insert_data.sql
@@ -0,0 +1,14 @@
+INSERT INTO customers VALUES
+(1,'2024-01-05','Instagram'),
+(2,'2024-01-07','Google Ads'),
+(3,'2024-01-10','Referral');
+
+INSERT INTO website_events VALUES
+(101,1,'visit','2024-02-01'),
+(102,1,'add_to_cart','2024-02-01'),
+(103,1,'purchase','2024-02-01'),
+(104,2,'visit','2024-02-02');
+
+INSERT INTO orders VALUES
+(201,1,'2024-02-01',1200,10),
+(202,1,'2024-03-01',950,0);
